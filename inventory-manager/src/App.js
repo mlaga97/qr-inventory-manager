e717708e4dc43fe12a07264cb34f83a88f1f573c
Luc Lagarde <matieu.lagarde@gmail.com>
2021-04-26 03:37:37 -0500

    Add table

--- inventory-manager/src/App.js
+++ inventory-manager/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Navbar, Card, Container, Row, Col, Form, Button} from 'react-bootstrap';
+import {Navbar, Card, Container, Row, Col, Form, Button, Table} from 'react-bootstrap';
 import PouchDB from 'pouchdb';
 
 import QRCodeReader from './QRCodeReader';
@@ -70,6 +70,8 @@ class RenderUUID extends React.Component {
     }).catch((e) => {
       console.log(e);
     });
+
+    this.props.callback(this.props.uuid);
   }
 
   render() {
@@ -79,23 +81,84 @@ class RenderUUID extends React.Component {
     if (this.state.error)
       return this.state.error.reason;
 
-    return <Card style={{'width': '100%', 'margin': '10px'}} >
-      <Card.Header>Editing {this.props.uuid}</Card.Header>
-      <Card.Body>
-        <Card.Text>
-          <Form onSubmit={this.handleSubmit} >
-            <Form.Group controlId='label'>
-              <Form.Label>Label</Form.Label>
-              <Form.Control name='label' value={this.state.label} onChange={this.handleChange} />
-            </Form.Group>
-            <Form.Group controlId='labelPrinted'>
-              <Form.Check name='labelPrinted' label='Label Printed' value={this.state.labelPrinted} onChange={this.handleChange} />
-            </Form.Group>
-            <Button variant='primary' type='submit'>Submit</Button>
-          </Form>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    return <>
+      <Card style={{'width': '100%', 'margin': '10px'}} >
+        <Card.Header>Editing {this.props.uuid}</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <Form onSubmit={this.handleSubmit} >
+              <Form.Group controlId='label'>
+                <Form.Label>Label</Form.Label>
+                <Form.Control name='label' value={this.state.label} onChange={this.handleChange} />
+              </Form.Group>
+              <Form.Group controlId='labelPrinted'>
+                <Form.Check name='labelPrinted' label='Label Printed' value={this.state.labelPrinted} onChange={this.handleChange} />
+              </Form.Group>
+              <Button variant='primary' type='submit'>Submit</Button>
+            </Form>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  }
+}
+
+class RenderTable extends React.Component {
+  update() {
+    this.props.db.allDocs({
+      include_docs: true,
+    }).then((data) => {
+      this.setState(data);
+    }).catch((e) => {
+      console.log(e);
+    });
+  }
+
+  componentDidMount() {
+    this.update();
+  }
+
+  componentDidUpdate() {
+    this.update();
+  }
+
+  render() {
+    if (!this.state)
+      return null;
+
+    return <>
+      <Card style={{'width': '100%', 'margin': '10px'}} >
+        <Card.Header>All Inventory Items</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <Table>
+              <thead>
+                <tr>
+                  <th>UUID</th>
+                  <th>Label</th>
+                  <th>Label Printed</th>
+                  <th>Content</th>
+                  <th>Location</th>
+                  <th>Type</th>
+                  <th>Make/Model</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.rows.map((row) => <>
+                    <tr onClick={() => this.props.callback(row.doc._id)}>
+                      <td>{row.doc._id}</td>
+                      <td>{row.doc.label}</td>
+                      <td>{(row.doc.labelPrinted) ? 'Yes' : 'No'}</td>
+                    </tr>
+                  </>)
+                }
+              </tbody>
+            </Table>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>;
   }
 }
 
@@ -137,7 +200,8 @@ class App extends React.Component {
         <Container style={{'margin': '40px'}} >
           <Row>
             <QRReaderCard callback={this.update} />
-            <RenderUUID uuid={this.state.data} db={this.db} />
+            <RenderUUID uuid={this.state.data} callback={this.update} db={this.db} />
+            <RenderTable uuid={this.state.data} callback={this.update} db={this.db} />
           </Row>
         </Container>
 
